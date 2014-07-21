4cb9e2c897762e4e6148de26affde106ef8a79a4
Brian <briano@sachsmedia.com>
2014-07-21 14:56:32 -0400

    new main landing pg
    - removed buddy
    - fit proportionally without scroll on most screen sizes

--- wp-content/themes/ssk/page-templates/landing.php
+++ wp-content/themes/ssk/page-templates/landing.php
@@ -10,22 +10,22 @@ get_header('landing'); ?>
 
 <article class="logo">
 	<div class="col4 push4">
-		<img class="" src="<?php bloginfo('stylesheet_directory') ?>/assets/img/_landing-ssk-logo.png" />
+		<a href="<?php echo home_url(); ?>"><img class="" src="<?php bloginfo('stylesheet_directory') ?>/assets/img/_landing-ssk-logo.png" /></a>
 	</div>
 </article>
 
 
 <article class="thoughts">
 
-	<div class="col4 push0">
+	<div class="z col4 push0">
 		<a href="<?php echo home_url(); ?>/teachers"><img class="wow bounceIn" data-wow-duration="2s" data-wow-delay="0s" data-wow-iteration="1" src="<?php bloginfo('stylesheet_directory') ?>/assets/img/_landing-thought-teachers.png" /></a>
 	</div>
 	
-	<div class="col4 push4">
+	<div class="z col4 push4">
 		<a href="<?php echo home_url(); ?>/parents"><img class="wow bounceIn" data-wow-duration="2s" data-wow-delay="1s" src="<?php bloginfo('stylesheet_directory') ?>/assets/img/_landing-thought-parents.png" /></a>
 	</div>
 	
-	<div class="col4 push8">
+	<div class="z col4 push8">
 		<a href="<?php echo home_url(); ?>/kids"><img class="wow bounceIn" data-wow-duration="2s"  data-wow-delay="2s" src="<?php bloginfo('stylesheet_directory') ?>/assets/img/_landing-thought-kids.png" /></a>
 	</div>
 	
@@ -43,14 +43,11 @@ get_header('landing'); ?>
 
 <article class="footer">
 	<div class="col12 push0">
-		<img class="wow fadeInUp buddy" data-wow-duration="1s" data-wow-delay="0s" src="<?php bloginfo('stylesheet_directory') ?>/assets/img/_landing-buddy.gif" />
-	</div>
-</article>
-
-<article class="music">
-	<div class="col12 push0 audio-player">
-	 <h2></h2>
-	 <audio id="audio-player"autoplay="autoplay" loop="loop" src="<?php bloginfo('stylesheet_directory') ?>/assets/music/ssk-theme.mp3" type="audio/mp3" controls="controls"></audio>
+		<img id="ssk" src="<?php bloginfo('stylesheet_directory') ?>/assets/img/bg-landingv2.jpg" />
+		<div class="col12 push0 audio-player">
+		 <h2></h2>
+		 <audio id="audio-player"autoplay="autoplay" loop="loop" src="<?php bloginfo('stylesheet_directory') ?>/assets/music/ssk-theme.mp3" type="audio/mp3" controls="controls"></audio>
+		</div>
 	</div>
 </article>
 
