2c2d90e6d217ad5e928222b5142cb07d15a52bbc
Mike <mike@sachsmedia.com>
2016-05-20 17:19:10 -0400

    google play badge, sidebar button

--- wp-content/themes/ssk/page-templates/kids.php
+++ wp-content/themes/ssk/page-templates/kids.php
@@ -25,7 +25,10 @@ Template Name: Kids Landing pg
 			<div class="entry-content clearfix">
 				<?php the_content(); ?>
 				<a href="https://itunes.apple.com/us/app/laurens-kingdom/id1028748234?mt=8" class="app-download">
-					<img class="itunes" src="<?= get_stylesheet_directory_uri(); ?>/assets/img/Download_on_the_App_Store_Badge_US-UK_135x40.svg">
+					<img class="itunes" src="<?= get_stylesheet_directory_uri(); ?>/assets/img/Download_on_the_App_Store_Badge_US-UK_135x40.svg" alt="Download on the App Store">
+				</a>
+				<a href="https://play.google.com/store/apps/details?id=com.RapptorStudios.LaurensKingdom" class="app-download">
+					<img class="itunes" src="<?= get_stylesheet_directory_uri(); ?>/assets/img/en_badge_web_generic.png" alt="Download on Google Play">
 				</a>
 			</div>
 
